6e4dcda78435c651fc886e7e814c58eb4a4e7467
Ruben <rubengorodri@gmail.com>
2023-12-21 19:05:35 +0100

    main forced merge

--- capaDomini.cpp
+++ capaDomini.cpp
@@ -12,19 +12,9 @@ void capaDomini::registrarUsuari(string nom, string sobrenom, string contrasenya
 	passarelaUsuari usuari(nom, sobrenom, contrasenya, correuE, dataN);
 	usuari.insereix();
 }
-<<<<<<< HEAD:capes/capaDomini.cpp
-/*
-pqxx::result capaDomini::totsVideojocs() {
-	//cercadoraVideojocs videojoc();
-	//return videojoc.totsVideojocs
-	//return pqxx::result result();
-}
-*/
-=======
 
 pqxx::result capaDomini::totsVideojocs() {
 	//cercadoraVideojocs videojoc();
 	//return videojoc.totsVideojocs
 	return pqxx::result result();
 }
->>>>>>> 5a95daf21fd6fbbd077a1b75075f8bc767c02a5d:capaDomini.cpp
--- cercadoraUsuari.h
+++ cercadoraUsuari.h
@@ -1,10 +1,6 @@
 #include<pqxx/pqxx>
 #include<iostream>
-<<<<<<< HEAD:cercadores/cercadoraUsuari.h
-#include "../passareles/passarelaUsuari.h"
-=======
 #include "passareles/passarelaUsuari.h"
->>>>>>> 5a95daf21fd6fbbd077a1b75075f8bc767c02a5d:cercadoraUsuari.h
 
 class cercadoraUsuari
 {
