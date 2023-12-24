9404f091680da916cc5695e305259f8b8538443e
PauRF03 <pauramos2003@gmail.com>
2023-12-24 19:17:31 +0100

    add includes

--- capaPresentacio.h
+++ capaPresentacio.h
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <stdlib.h>
-#include "transaccions/capaDomini.h"
+#include <pqxx/pqxx>
+#include "transaccions/TXcomprarPaquet.h"
+#include "transaccions/TXcomprarVideojoc.h"
+#include "transaccions/TXconsultarCompres.h"
+#include "transaccions/TXconsultarNovetats.h"
+#include "transaccions/TXconsultarPaquet.h"
+#include "transaccions/TXconsultarPaquets.h"
+#include "transaccions/TXconsultarUsuari.h"
+#include "transaccions/TXconsultarVideojoc.h"
+#include "transaccions/TXconsultarVideojocs.h"
+#include "transaccions/TXconsultarVideojocsPerEdat.h"
+#include "transaccions/TXesborrarUsuari.h"
+#include "transaccions/TXiniciarSessio.h"
+#include "transaccions/TXregistrarUsuari.h"
+#include "transaccions/TXtancarSessio.h"
 
 using namespace std;
 
