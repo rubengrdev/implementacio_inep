e5fe939b2a2469c2673a81bf47c7195f6a385516
Pau Alcázar <pau.alcazar11@gmail.com>
2023-12-31 13:26:57 +0100

    pseudocodi de les transaccions de compra

--- transaccions/TXcomprarPaquet.cpp
+++ transaccions/TXcomprarPaquet.cpp
@@ -1,15 +1,42 @@
 #include "TXcomprarPaquet.h"
+#include "cercadoraElementCompra.h"
+#include "cercadoraUsuari.h"
+// Fan falta més includes?
 
 TXcomprarPaquet::TXcomprarPaquet() {
-
+    // Constructor
 }
 
 TXcomprarPaquet::~TXcomprarPaquet() {
-
+    // Destructor
 }
 
-void TXcomprarPaquet::executar(){
+void TXcomprarPaquet::executar() {
+    // Suposem que tenim un nom d'usuari i un nom de paquet com a atributs de la classe
+    cercadoraElementCompra cercadorEl = cercadoraElementCompra();
+    cercadoraUsuari cercadorUs = cercadoraUsuari();
 
+    // Verificar existència del paquet
+    passarelaElementCompra paquet = cercadorEl.cercaPerNom(nomPaquet);
+    if (!paquet.isValid()) {
+        throw std::runtime_error("Paquet no trobat");
+    }
+
+    // Verificar usuari
+    passarelaUsuari usuari = cercadorUs.cercaPerSobrenom(nomUsuari);
+    if (!usuari.isValid()) {
+        throw std::runtime_error("Usuari no trobat");
+    }
+
+    // Aquí s'ha de gestionar la lògica de la compra.
+    // Per exemple, registrar la compra en una taula de compres, actualitzar estocs, etc.
+
+    // Commit de la transacció, o rollback en cas d'error.
 }
 
-//void TXcomprarPaquet::resultat
+// Aquest mètode pot retornar informació sobre el resultat de la transacció
+// Depèn del que vulguis retornar
+// per exemple, si la compra ha estat exitosa, detalls de la compra, etc.
+TXcomprarPaquet::Res TXcomprarPaquet::obteResultat() {
+    return resultat;
+}
--- transaccions/TXcomprarVideojoc.cpp
+++ transaccions/TXcomprarVideojoc.cpp
@@ -1,15 +1,42 @@
 #include "TXcomprarVideojoc.h"
+#include "cercadoraElementCompra.h"
+#include "cercadoraUsuari.h"
 
 TXcomprarVideojoc::TXcomprarVideojoc() {
-
+    // Constructor
 }
 
 TXcomprarVideojoc::~TXcomprarVideojoc() {
-
+    // Destructor
 }
 
-void TXcomprarVideojoc::executar(){
+void TXcomprarVideojoc::executar() {
+    // Suposem que tenim un nom d'usuari i un nom de videojoc com a atributs de la classe
+    cercadoraElementCompra cercadorEl = cercadoraElementCompra();
+    cercadoraUsuari cercadorUs = cercadoraUsuari();
 
+    // Verificar existència del videojoc
+    passarelaElementCompra videojoc = cercadorEl.cercaPerNom(nomVideojoc);
+    if (!videojoc.isValid()) {
+        throw std::runtime_error("Videojoc no trobat");
+    }
+
+    // Verificar usuari
+    passarelaUsuari usuari = cercadorUs.cercaPerSobrenom(nomUsuari);
+    if (!usuari.isValid()) {
+        throw std::runtime_error("Usuari no trobat");
+    }
+
+    // Aquí s'ha de gestionar la lògica de la compra del videojoc.
+    // Això podria incloure registrar la compra a la base de dades, verificar saldos, etc.
+    // Aquesta part depèn de l'esquema i la lògica de negoci del teu sistema.
+
+    // Commit de la transacció, o rollback en cas d'error.
 }
 
-//void TXcomprarVideojoc::resultat
+// Aquest mètode pot retornar informació sobre el resultat de la transacció
+// Depèn del que vulguis retornar
+// per exemple, si la compra ha estat exitosa, detalls de la compra, etc.
+TXcomprarVideojoc::Res TXcomprarVideojoc::obteResultat() {
+    return resultat;
+}
