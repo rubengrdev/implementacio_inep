47ea5b9e76fe18408ae0fb6b4b800370be40e14d
Pau Alcázar <pau.alcazar11@gmail.com>
2023-12-31 13:36:14 +0100

    més transaccions

--- transaccions/TXconsultarCompres.cpp
+++ transaccions/TXconsultarCompres.cpp
@@ -1,15 +1,33 @@
 #include "TXconsultarCompres.h"
+#include "cercadoraCompra.h"
+#include <vector>
 
 TXconsultarCompres::TXconsultarCompres() {
-
+    // Constructor
 }
 
 TXconsultarCompres::~TXconsultarCompres() {
-
+    // Destructor
 }
 
-void TXconsultarCompres::executar(){
+void TXconsultarCompres::executar() {
+    cercadoraCompra cercador = cercadoraCompra();
+    // Aquí pots afegir el codi per a cercar compres.
+    // Per exemple, cercar totes les compres d'un usuari específic.
 
+    // Suposem que tenim un mètode per obtenir totes les compres
+    vector<passarelaCompra> compres = cercador.totesLesCompres();
+
+    for (const auto& compra : compres) {
+        // Processar cada compra i afegir al resultat
+        res r;
+        r.nomVideojoc = compra.getNomVideojoc();
+        r.preu = compra.getPreu();
+        r.data = compra.getData();
+        resultat.push_back(r);
+    }
 }
 
-//void TXconsultarCompres::resultat
+vector<TXconsultarCompres::res> TXconsultarCompres::obteResultat() {
+    return resultat;
+}
--- transaccions/TXconsultarNovetats.cpp
+++ transaccions/TXconsultarNovetats.cpp
@@ -1,15 +1,33 @@
 #include "TXconsultarNovetats.h"
+#include "cercadoraElementCompra.h"
+#include <vector>
 
 TXconsultarNovetats::TXconsultarNovetats() {
-
+    // Constructor
 }
 
 TXconsultarNovetats::~TXconsultarNovetats() {
-
+    // Destructor
 }
 
-void TXconsultarNovetats::executar(){
+void TXconsultarNovetats::executar() {
+    cercadoraElementCompra cercador = cercadoraElementCompra();
+    // Aquí pots afegir el codi per a cercar les novetats.
+    // Per exemple, cercar tots els videojocs nous o els paquets recents.
 
+    // Suposem que tenim un mètode per obtenir les novetats
+    vector<passarelaElementCompra> novetats = cercador.novetats();
+
+    for (const auto& novetat : novetats) {
+        // Processar cada novetat i afegir al resultat
+        res r;
+        r.nom = novetat.getNom();
+        r.desc = novetat.getDescripcio();
+        r.preu = novetat.getPreu();
+        resultat.push_back(r);
+    }
 }
 
-//void TXconsultarNovetats::resultat
+vector<TXconsultarNovetats::res> TXconsultarNovetats::obteResultat() {
+    return resultat;
+}
