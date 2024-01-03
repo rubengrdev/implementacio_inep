77c1caf92e0d33088bdf2f9f04d0de96c3335650
Pau Alcázar <pau.alcazar11@gmail.com>
2024-01-03 21:05:46 +0100

    TXconsulyarUsuari

--- transaccions/TXconsultarUsuari.cpp
+++ transaccions/TXconsultarUsuari.cpp
@@ -1,15 +1,31 @@
 #include "TXconsultarUsuari.h"
 
 TXconsultarUsuari::TXconsultarUsuari() {
-
+    // Constructor
 }
 
 TXconsultarUsuari::~TXconsultarUsuari() {
-
+    // Destructor
 }
 
-void TXconsultarUsuari::executar(){
+void TXconsultarUsuari::executar() {
+    Videoconsola& vid = Videoconsola::getInstance();
+    string sn = vid.getUsuari();
+    cercadoraUsuari us = cercadoraUsuari();
+    passarelaUsuari pus = us.cercaPerSobrenom(sn);
 
+    // Comprovem si l'usuari existeix
+    if (!pus.isValid()) {
+        throw exception("Usuari no trobat");
+    }
+
+    // Emmagatzemar les dades de l'usuari en la variable 'resultat'
+    resultat.nom = pus.getNom();
+    resultat.sobrenom = pus.getSobrenom();
+    resultat.correu = pus.getCorreu();
+    resultat.dataNaixement = pus.getDataNaixement();
 }
 
-//void TXiniciarSessio::resultat
+TXconsultarUsuari::res TXconsultarUsuari::obteResultat() {
+    return resultat;
+}
