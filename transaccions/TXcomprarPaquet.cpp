a8c270d6ca2e0ff6bf6528e0201d1246696a867a
Pau Alcázar <pau.alcazar11@gmail.com>
2024-01-03 20:23:51 +0100

    canvis en les transaccions de comprar paquet i comprar  videojoc

--- transaccions/TXcomprarPaquet.cpp
+++ transaccions/TXcomprarPaquet.cpp
@@ -1,21 +1,29 @@
 #include "TXcomprarPaquet.h"
+#include "cercadoraElementCompra.h"
+#include "cercadoraPaquetVideojocs.h"
+#include "Videoconsola.h"
+#include "passarelaCompra.h"
+#include <ctime>
+#include <string>
 
-TXcomprarPaquet::TXcomprarPaquet(string nom) {
-    nomPaquet = nom;
+TXcomprarPaquet::TXcomprarPaquet(string nom) : nomPaquet(nom) {
+    // Constructor
 }
 
 TXcomprarPaquet::~TXcomprarPaquet() {
-    
+    // Destructor
 }
 
 void TXcomprarPaquet::executar() {
-    // Suposem que tenim un nom d'usuari i un nom de paquet com a atributs de la classe
     cercadoraElementCompra cercadorEl = cercadoraElementCompra();
     cercadoraPaquetVideojocs cercadorPa = cercadoraPaquetVideojocs();
 
-    // Verificar existència del paquet
     passarelaElementCompra el = cercadorEl.cercaPerNom(nomPaquet);
     passarelaPaquetVideojocs pa = cercadorPa.cercaPerNom(nomPaquet);
+
+    if (!el.isValid() || !pa.isValid()) {
+        throw std::runtime_error("Paquet no trobat");
+    }
 
     Videoconsola& v = Videoconsola::getInstance();
     string usuari = v.getUsuari();
@@ -23,9 +31,14 @@ void TXcomprarPaquet::executar() {
     time_t ara = time(0);
     struct tm temps;
     localtime_s(&temps, &ara);
-    string data = to_string(temps.tm_year) + "-" + to_string(temps.tm_mon) + "-" + to_string(temps.tm_mday);
+    string data = std::to_string(temps.tm_year + 1900) + "-" + std::to_string(temps.tm_mon + 1) + "-" + std::to_string(temps.tm_mday);
+    
     passarelaCompra compra = passarelaCompra(usuari, nomPaquet, data, el.getPreu());
     compra.insereix();
+
+    // Potser cal actualitzar l'estat de l'usuari o del paquet després de la compra.
+
+    resultat = true; // Suposant que resultat és un booleà que indica si la transacció ha estat exitosa.
 }
 
 vector<TXcomprarPaquet::res> TXcomprarPaquet::obteResultat() {
--- transaccions/TXcomprarVideojoc.cpp
+++ transaccions/TXcomprarVideojoc.cpp
@@ -1,8 +1,13 @@
 #include "TXcomprarVideojoc.h"
+#include "cercadoraElementCompra.h"
+#include "cercadoraVideojoc.h"
+#include "Videoconsola.h"
+#include "passarelaCompra.h"
+#include <ctime>
+#include <string>
 
-TXcomprarVideojoc::TXcomprarVideojoc(string n) {
+TXcomprarVideojoc::TXcomprarVideojoc(string n) : nom(n) {
     // Constructor
-    nom = n;
 }
 
 TXcomprarVideojoc::~TXcomprarVideojoc() {
@@ -10,30 +15,32 @@ TXcomprarVideojoc::~TXcomprarVideojoc() {
 }
 
 void TXcomprarVideojoc::executar() {
-    // Suposem que tenim un nom d'usuari i un nom de videojoc com a atributs de la classe
     cercadoraElementCompra cercadorEl = cercadoraElementCompra();
     cercadoraVideojoc cercadorVid = cercadoraVideojoc();
     Videoconsola& consola = Videoconsola::getInstance();
     string usuari = consola.getUsuari();
+
     passarelaVideojoc pvid = cercadorVid.cercaPerNom(nom);
     passarelaElementCompra pel = cercadorEl.cercaPerNom(nom);
+
+    if (!pvid.isValid() || !pel.isValid()) {
+        throw std::runtime_error("Videojoc no trobat");
+    }
+
     time_t ara = time(0);
     struct tm temps;
     localtime_s(&temps, &ara);
-    string data = to_string(temps.tm_year) + "-" + to_string(temps.tm_mon) + "-" + to_string(temps.tm_mday);
+    string data = std::to_string(temps.tm_year + 1900) + "-" + std::to_string(temps.tm_mon + 1) + "-" + std::to_string(temps.tm_mday);
+
     passarelaCompra compra = passarelaCompra(usuari, nom, data, pel.getPreu());
     compra.insereix();
 
-    // Aquí s'ha de gestionar la lògica de la compra del videojoc.
-    // Això podria incloure registrar la compra a la base de dades, verificar saldos, etc.
-    // Aquesta part depèn de l'esquema i la lògica de negoci del teu sistema.
+    //potser caldria actualitzar l'estat de l'usuari o del videojoc.
 
-    // Commit de la transacció, o rollback en cas d'error.
+    resultat = true;  // Suposant que resultat és un booleà que indica si la transacció ha estat exitosa.
 }
 
-// Aquest mètode pot retornar informació sobre el resultat de la transacció
-// Depèn del que vulguis retornar
-// per exemple, si la compra ha estat exitosa, detalls de la compra, etc.
-TXcomprarVideojoc::res TXcomprarVideojoc::obteResultat() {
+bool TXcomprarVideojoc::obteResultat() {
     return resultat;
 }
+
